bb3356f31f4bd77ff6c5c23cd5c2947462343f80
Hanhtunaung97 <littlehanhtunaung97>
2024-05-30 20:44:20 +0800

    book list render and UI setup

--- src/components/BookList.components.jsx
+++ src/components/BookList.components.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const BookListComponents = () => {
+const BookListComponents = ({ data }) => {
   return (
-    <div>BookListComponents</div>
-  )
-}
+    <div>
+      <div className=" w-full border border-violet-300 rounded-none h-[550px] hover:scale-95 duration-200">
+        <img
+          src={data.image}
+          className="h-3/5 object-top object-cover w-full"
+          alt="novelsImage"
+        />
+        <div className=" p-3">
+          <div className="mb-3">
+            <h3 className=" text-xl font-semibold text-violet-700">{data.book}</h3>
+            <h6 className=" text-sm text-violet-500">{data.author}</h6>
+          </div>
+          <div>
+            <p className=" text-slate-700 font-bold">{data.slug}</p>
+            <p className=" text-slate-500">{data.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default BookListComponents
+export default BookListComponents;
--- src/page/Home.page.jsx
+++ src/page/Home.page.jsx
@@ -3,6 +3,7 @@ import useFetch from "../hook/useFetch";
 import { getBookUrl } from "../service/booksUrl.service";
 import Loading from "../Loading";
 import BookListComponents from "../components/BookList.components";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   const { data, error, loading } = useFetch(getBookUrl, "books");
@@ -15,9 +16,11 @@ const HomePage = () => {
           {error ? (
             <h1>{error}</h1>
           ) : (
-            <div>
+            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-5">
               {data.map((el) => (
-                <BookListComponents key={el.id} data={el} />
+                <Link key={el.id}>
+                  <BookListComponents data={el} />
+                </Link>
               ))}
             </div>
           )}
